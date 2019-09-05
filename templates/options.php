3e3375147565e0d3370448d26f6526983c1a064f
Christoph Arndt <christoph.arndt@greenpeace.org>
2019-09-05 11:47:38 +0200

    Create options page template

---
+++ templates/options.php
@@ -0,0 +1,13 @@
+<div class="wrap">
+	<h2><?php _e( 'RaiseNow Lema Widget Setup', P4_GPCH_RNLW_PREFIX ); ?></h2>
+
+	<form action="options.php" method="post">
+
+		<?php
+		// settings_fields( P4_GPCH_RNLW_PREFIX . '_donation_settings' );
+		// do_settings_sections( P4_GPCH_RNLW_PREFIX . '_donation_settings' );
+		submit_button();
+		?>
+
+	</form>
+</div>
