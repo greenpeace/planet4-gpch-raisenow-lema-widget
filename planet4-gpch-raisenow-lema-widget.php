bd062b8a29cb7ee3bf2e0bca579b52fb7818a568
Christoph Arndt <christoph.arndt@greenpeace.org>
2019-09-05 11:30:19 +0200

    Create plugin php file and add plugin header comment

---
+++ planet4-gpch-raisenow-lema-widget.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Planet4 GPCH RaiseNow Lema Widget
+ * Plugin URI: https://github.com/greenpeace/planet4-gpch-raisenow-lema-widget
+ * Description: A WordPress Plugin to embed the RaiseNow Lema Widget with a Gutenberg Block.
+ * Version: 0.1.0
+ * Requires at least: 5.2.2
+ * Requires PHP: 7.0.33
+ * Author: Greenpeace Switzerland
+ * Author URI: https://www.greenpeace.ch
+ * License: MIT
+ * License URI: https://opensource.org/licenses/MIT
+ * Text Domain: planet4-gpch-raisenow-lema-widget
+ */
